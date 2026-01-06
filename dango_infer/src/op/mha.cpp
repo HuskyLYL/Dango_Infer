2957a7c0e6e8ee32547e680f7c6ff55a9e87e20c
HuskyLYL <1308951103@qq.com>
2026-01-06 15:55:14 +0800

    commit

--- dango_infer/src/op/mha.cpp
+++ dango_infer/src/op/mha.cpp
@@ -135,13 +135,13 @@ namespace op
         else if (data_type == base::DataType::kDataTypeBf16)
         {
             //好了,这里集成进来了
-            //flashinfer::get_single_decode_with_kv_cache_kernel()(pos_, head_num_, layer_index_, seq_len_, kv_dim_, kv_mul_,
-            //    head_size_, mha_out, query_tensor, score_tensor,
-            //    key_cache_tensor, value_cache_tensor, stream);
-
-            bf16x8_kernel_cu::get_mha_kernel()(pos_, head_num_, layer_index_, seq_len_, kv_dim_, kv_mul_,
+            flashinfer::get_single_decode_with_kv_cache_kernel()(pos_, head_num_, layer_index_, seq_len_, kv_dim_, kv_mul_,
                 head_size_, mha_out, query_tensor, score_tensor,
                 key_cache_tensor, value_cache_tensor, stream);
+
+            //bf16x8_kernel_cu::get_mha_kernel()(pos_, head_num_, layer_index_, seq_len_, kv_dim_, kv_mul_,
+            //    head_size_, mha_out, query_tensor, score_tensor,
+            //    key_cache_tensor, value_cache_tensor, stream);
 
 
         }
