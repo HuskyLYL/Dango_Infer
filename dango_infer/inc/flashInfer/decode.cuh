a980249915bb4e2906f0556064a895b7853e775a
HuskyLYL <1308951103@qq.com>
2026-01-08 15:43:46 +0800

    commit

--- dango_infer/inc/flashInfer/decode.cuh
+++ dango_infer/inc/flashInfer/decode.cuh
@@ -361,7 +361,7 @@ namespace flashinfer
 
         //const uint32_t seq_len = kv_len;
 
-        constexpr uint32_t vec_size = 32UL / sizeof(T);
+        constexpr uint32_t vec_size = 8UL / sizeof(T);
         const uint32_t bdx = head_dim/ vec_size;
         auto compute_capacity = GetCudaComputeCapability();
         //一个head_dim 要做归约操作，不能让他的值大于32了
