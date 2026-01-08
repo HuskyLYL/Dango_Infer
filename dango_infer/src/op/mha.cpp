f25c5fca648147e48126c7e358e775afaee80880
HuskyLYL <1308951103@qq.com>
2026-01-08 10:23:32 +0800

    commit

--- dango_infer/src/op/mha.cpp
+++ dango_infer/src/op/mha.cpp
@@ -136,7 +136,7 @@ namespace op
         {
             //好了,这里集成进来了
 
-            LOG(INFP) << "into_single_decode_with_kv_cache_kernel.";
+            LOG(INFO) << "into_single_decode_with_kv_cache_kernel.";
             flashinfer::get_single_decode_with_kv_cache_kernel()(pos_, head_num_, layer_index_, seq_len_, kv_dim_, kv_mul_,
                 head_size_, mha_out, query_tensor, score_tensor,
                 key_cache_tensor, value_cache_tensor, stream);
