ad2c617668ed81a226bd03e70eedfba971e87991
HuskyLYL <1308951103@qq.com>
2026-01-15 20:53:56 +0800

    commit

--- sampler/tp_parallel_simple_chat/main.cpp
+++ sampler/tp_parallel_simple_chat/main.cpp
@@ -131,7 +131,7 @@ int main(int argc, char* argv[])
     
     printf("RANK[%d]Generating...\n",nccl::G_MPI_RANK);
     fflush(stdout);
-    int steps = generate(model, sentence, 100, true);
+    int steps = generate(model, sentence, 2, true);
     auto end = std::chrono::steady_clock::now();
     auto duration = std::chrono::duration<double>(end - start).count();
     printf("\nsteps/s:%lf\n", static_cast<double>(steps) / duration);
